9b7ef26b99b28b1d2b6105177b1417e2cf09b3fe
AdityaShah805 <67499202+AdityaShah805@users.noreply.github.com>
2020-09-13 15:13:57 +0530

    Add files via upload

---
+++ GALAXY EXPLORATION/code.js
@@ -0,0 +1,51 @@
+onEvent("button1", "click", function(event) {
+   setScreen("learnscreen");
+});
+onEvent("button5", "click", function(event) {
+   setScreen("subject1");
+});
+onEvent("button13", "click", function(event) {
+   setScreen("subject2");
+});
+onEvent("button11", "click", function(event) {
+   setScreen("subject3");
+});
+onEvent("button12", "click", function(event) {
+   setScreen("subject4");
+});
+onEvent("button10", "click", function(event) {
+   setScreen("screen1");
+});
+onEvent("button2", "click", function(event) {
+   setScreen("learnscreen");
+});
+onEvent("button14", "click", function(event) {
+   setScreen("screen2");
+});
+onEvent("button3", "click", function(event) {
+   setScreen("learnscreen");
+});
+onEvent("button15", "click", function(event) {
+   setScreen("screen3");
+});
+onEvent("button4", "click", function(event) {
+   setScreen("learnscreen");
+});
+onEvent("button19", "click", function(event) {
+   setScreen("screen4");
+});
+onEvent("button2", "click", function(event) {
+   setScreen("button6");
+});
+onEvent("button6", "click", function(event) {
+   setScreen("learnscreen");
+});
+onEvent("button9", "click", function(event) {
+   setScreen("welcomescreen");
+});
+onEvent("button7", "click", function(event) {
+   setScreen("howtouse");
+});
+onEvent("button8", "click", function(event) {
+   setScreen("welcomescreen");
+});
